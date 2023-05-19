d41380b7d8b4016148aa3b5d3f6a20788567b496
karladev9 <carlychan14@gmail.com>
2023-05-19 10:50:47 -0600

    SEGUNDO INTENTO

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,47 @@
+
+
 const cipher = {
-  // ...
-};
+
+    encode: function cifrar(offset, string) {
+        let resultado = "";
+        let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+        offset = (offset % 26 + 26) % 26;
+        if (string) {
+            for (let i = 0; i < string.length; i++) {
+
+                if (letras.indexOf(string[i]) != -1) {
+                    let posicion = ((letras.indexOf(string[i]) + offset) % 26);
+                    resultado += letras[posicion];
+                } else {
+                    resultado += string[i];
+                }
+            }
+            return resultado;
+        }
+    },
+
+    decode: function descifrar(offset, string) {
+        let resultado = "";
+        let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        offset = (offset % 26 + 26) % 26;
+
+        if (string) {
+            for (let i = 0; i < string.length; i++) {
+                if (letras.indexOf(string[i]) != -1) {
+                    let posicion = ((letras.indexOf(string[i]) - offset) % 26);
+                    resultado += letras[posicion];
+                } else {
+                    resultado += string[i];
+                }
+            }
+            return resultado;
+        }
+    }
+}
+
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
+
 import cipher from './cipher.js';
+//window.addEventListener("load", function, true)
+
+//Constantes del HTML que atraen la informacion y la guardan--
+const botonMensaje = document.getElementById("mensaje");
+const botonCifrado = document.getElementById("cifrar");
+const botonDesifrado = document.getElementById("descifrar");
+
+//Uso del addEventListener--
+
+botonMensaje.addEventListener("keyup", function () {
+
+    let msja = document.getElementById("mensaje").value;
+    msja.toUpperCase(); 
+    document.getElementById("mensaje").value = msja.toUpperCase();
+}, true);
+
+botonCifrado.addEventListener("click", function () {
+    let offset = document.getElementById("desplazamiento").value;
+    let string = document.getElementById("mensaje").value;
+    
+    document.getElementById("mensaje2").value = cipher.encode(offset, string); 
+}, true);
+
+botonDesifrado.addEventListener("click", function () {
+    let string = document.getElementById("mensaje").value;
+    let offset = document.getElementById("desplazamiento").value;
+    document.getElementById("mensaje2").value = cipher.decode(offset, string);
+
+}, true);
+
+
+
+
+
+
+//function inicio(){
+
+// document.getElementById("mensaje").addEventListener("keyUp", function(){
+//        this.value = this.value.toUpperCase();
+// }, true);
+
+/*document.getElementById("cifrar").addEventListener("click", function(){
+               
+   
+}, true);
+
+document.getElementById("descifrar").addEventListener("click", function(){
+                let string = document.getElementById("mensaje").value; 
+                let offset = document.getElementById("desplazamiento").value; 
+    document.getElementById("mensaje2").value = descifrar(string, offset);
+}, true);
+//}*/
 
 console.log(cipher);
